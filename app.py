2d76103d6de429f79bc0a2839c84f7e41a9e99d3
97Rafa <raf.samonis4@gmail.com>
2024-10-24 20:11:46 +0300

    Estimation Array has changed Logic

--- app.py
+++ app.py
@@ -2,36 +2,18 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
 from os import environ
-# from config import DB_CONFIG
+from datetime import timedelta
+from models import db, Requests, Estimations, create_tables
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
-db = SQLAlchemy(app)
 
+# Initialize the database
+db.init_app(app)
 
-class Kafka_Req(db.Model):
-    __tablename__ = 'kafka_reqs'
+# Create tables before starting the app
+create_tables(app)
 
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(30), nullable=False, default='pending')
-    req_type = db.Column(db.String(30), nullable=False)
-    body = db.Column(JSON, nullable=False)
-
-    def toJson(self):
-        return {'id' : self.id, 'status' : self.status, 'req_type' : self.req_type ,'body' : self.body}
-    
-class Kafka_Est(db.Model):
-    __tablename__ = 'kafka_ests'
-
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(30), nullable=True, default='completed')
-    body = db.Column(JSON, nullable=False)
-
-    def toJson(self):
-        return {'id' : self.id, 'status' : self.status,'body' : self.body}
-    
-with app.app_context():
-        db.create_all()
 
 #create a test route
 @app.route('/test', methods=['GET'])
@@ -44,11 +26,14 @@ def test():
 
 
 #create a kafka request
-@app.route('/kafka_reqs', methods=['POST'])
+@app.route('/requests', methods=['POST'])
 def create_kafka_req():
     try:
         data = request.get_json()
-        new_kafka_req = Kafka_Req(req_type=data['req_type'], body=data['body'])
+        new_kafka_req = Requests(
+            req_type=data['req_type'],
+            body=data['body']
+            )
         db.session.add(new_kafka_req)
         db.session.commit()
         return make_response(jsonify({'message' : 'Kafka request created'}), 201)
@@ -56,11 +41,11 @@ def create_kafka_req():
         return make_response(jsonify({'message' : 'Error creating request'}), 500)
 
 #get all requests
-@app.route('/kafka_reqs', methods=['GET'])
+@app.route('/requests', methods=['GET'])
 def get_kafka_reqs():
     try:
          # Query the database for all items
-        kafka_requests = Kafka_Req.query.all()
+        kafka_requests = Requests.query.all()
         # Convert the result to a list of dictionaries
         kafka_requests_l = [kafka_request.toJson() for kafka_request in kafka_requests]
         return make_response(jsonify(kafka_requests_l), 200)
@@ -69,10 +54,10 @@ def get_kafka_reqs():
         return make_response(jsonify({'message' : 'Error getting requests'}), 500)
 
 #get a request by id
-@app.route('/kafka_reqs/<int:id>', methods=['GET'])
+@app.route('/requests/<int:id>', methods=['GET'])
 def get_kafka_req(id):
     try:
-        kafka_req = Kafka_Req.query.filter_by(id=id).first()
+        kafka_req = Requests.query.filter_by(id=id).first()
         if kafka_req:
             return make_response(jsonify({'Kafka Request' : kafka_req.toJson()}), 200)
         return make_response(jsonify({'message': 'Request not found'}), 404)
@@ -80,10 +65,10 @@ def get_kafka_req(id):
         return make_response(jsonify({'message' : 'Error getting request'}), 500)
     
 #delete a request by id
-@app.route('/kafka_reqs/<int:id>', methods=['DELETE'])
+@app.route('/requests/<int:id>', methods=['DELETE'])
 def delete_kafka_req(id):
     try:
-        kafka_req = Kafka_Req.query.filter_by(id=id).first()
+        kafka_req = Requests.query.filter_by(id=id).first()
         if kafka_req:
             db.session.delete(kafka_req)
             db.session.commit()
@@ -95,20 +80,37 @@ def delete_kafka_req(id):
 # =============================================
 # ========== Estimations table ================
 # =============================================
-#get all requests
-@app.route('/kafka_ests', methods=['GET'])
-def get_kafka_ests():
+def parse_interval(interval_str):
+    # This function converts a string like '5 minutes' into arguments for timedelta
+    time_units = {'seconds': 0, 'minutes': 0}
+
+    if 'minute' in interval_str:
+        time_units['minutes'] = int(interval_str.split()[0])
+
+    return time_units
+
+#get all estimations
+@app.route('/estimations', methods=['POST'])
+def get_ests():
     try:
-         # Query the database for all items
-        kafka_estimations = Kafka_Est.query.all()
-        # Convert the result to a list of dictionaries
-        kafka_estimations_l = [kafka_estimation.toJson() for kafka_estimation in kafka_estimations]
-        return make_response(jsonify(kafka_estimations_l), 200)
-        
+        data = request.get_json()
+
+        # Parse the duration from the JSON if it's provided
+        timeout = None
+        if 'timeout' in data:
+            timeout = data['timeout']  # This will be a string, e.g., '5 minutes'
+
+        new_estimation_req = Estimations(
+            synopsisID=data['synopsisID'], 
+            timeout=timedelta(**parse_interval(timeout)) if timeout else None  # Convert to timedelta
+            )
+        db.session.add(new_estimation_req)
+        db.session.commit()
+        return make_response(jsonify({'message' : 'Estimation request created'}), 201)
     except Exception:
-        return make_response(jsonify({'message' : 'Error getting estimations'}), 500)
+        return make_response(jsonify({'message' : 'Error asking for estimation'}), 500)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0' ,port='4000')
 
---
+++ models.py
@@ -0,0 +1,36 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON, INTERVAL
+
+db = SQLAlchemy()
+
+class Requests(db.Model):
+    __tablename__ = 'requests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(30), nullable=False, default='pending')
+    req_type = db.Column(db.String(30), nullable=False)
+    body = db.Column(JSON, nullable=False)
+
+    def toJson(self):
+        return {'id': self.id, 'status': self.status, 'req_type': self.req_type, 'body': self.body}
+
+class Estimations(db.Model):
+    __tablename__ = 'estimations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    synopsisID = db.Column(db.Integer, nullable=False)
+    timeout = db.Column(INTERVAL, nullable=False)
+    body = db.Column(JSON, nullable=True)
+    last_req = db.Column(db.DateTime(timezone=True), nullable=True)
+    last_data = db.Column(db.DateTime(timezone=True), nullable=True)
+    toRefresh = db.Column(db.Boolean, nullable=False, default=True)
+
+    def toJson(self):
+        return {'id': self.id, 'synpopsisID': self.synopsisID, 
+                'body': self.body, 'last_req': self.last_req, 'last_data': self.last_data, 'toRefresh': self.toRefresh}
+
+def create_tables(app):
+    """Function to create all tables"""
+    with app.app_context():
+        db.create_all()
